a9bea72c2b0380dd5984045861d159f319861746
Cecilia <85092507+ccs-ocampo@users.noreply.github.com>
2023-10-11 18:18:23 -0600

    Remove Cypress

---
+++ tests/basicLoginTemplate.js
@@ -0,0 +1,25 @@
+describe('Login', () => {
+  beforeEach(() => {
+    // Perform any setup tasks before each test case
+  })
+
+  it('should display login page', () => {
+    // Test case to verify if the login page is displayed correctly
+  })
+
+  it('should allow user to enter valid credentials', () => {
+    // Test case to verify if user can enter valid credentials and login successfully
+  })
+
+  it('should display error message for invalid credentials', () => {
+    // Test case to verify if error message is displayed for invalid credentials
+  })
+
+  it('should redirect to home page after successful login', () => {
+    // Test case to verify if user is redirected to home page after successful login
+  })
+
+  afterEach(() => {
+    // Perform any cleanup tasks after each test case
+  })
+})
---
+++ tests/basicTemplate.js
@@ -0,0 +1,21 @@
+describe('Test Suite Name', () => {
+  beforeEach(() => {
+    // Code to run before each test case
+  })
+
+  afterEach(() => {
+    // Code to run after each test case
+  })
+
+  it('Test Case 1', () => {
+    // Test case code
+  })
+
+  it('Test Case 2', () => {
+    // Test case code
+  })
+
+  it('Test Case 3', () => {
+    // Test case code
+  })
+})
